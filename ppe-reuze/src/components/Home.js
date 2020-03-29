57c39c110b4765136a4df9a93ffaaee1bef44f11
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-03-29 19:55:35 -0400

    Update home text

--- ppe-reuze/src/components/Home.js
+++ ppe-reuze/src/components/Home.js
@@ -6,6 +6,7 @@ export default function Home() {
         <div className="container">
             <h1 className="title">Reusing &amp; sanitizing PPE</h1>
             <h2 className="subtitle">Reuze is a platform for healthcare professionals to easily access community-sourced, evidence based strategies for sanitizing personal protect equipment in the COVID-19 epidemic.</h2>
+            <h2 className="subtitle">If you're a healthcare professional, sign up to submit your own strategies.</h2>
             <Link to="/start">
                 <button className="button is-rounded">Get Started</button>
             </Link>
