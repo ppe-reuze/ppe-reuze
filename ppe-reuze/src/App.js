a73ab63b393db176bf6b3a21370bc23582c5a358
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-03-29 00:54:04 -0400

    Set up React Router

--- ppe-reuze/src/App.js
+++ ppe-reuze/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.sass';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import SelectPPE from './components/SelectPPE';
@@ -8,21 +15,25 @@ import SelectPPE from './components/SelectPPE';
 function App() {
   return (
     <div className="App">
-      <section className="hero is-info is-fullheight">
-
-        <div className="hero-head">
-          <Navbar />
+      <Router>
+        <div>
+          <section className="hero is-info is-fullheight">
+              <div className="hero-head">
+                <Navbar />
+              </div>
+              <div className="hero-body">
+              <Switch>
+                <Route path="/">
+                  <SelectPPE />
+                </Route>
+              </Switch>
+              </div>
+              <div className="hero-foot">
+                <Footer />
+              </div>
+          </section>
         </div>
-
-        <div className="hero-body">
-          <SelectPPE />
-        </div>
-
-        <div className="hero-foot">
-          <Footer />
-        </div>
-
-      </section>
+      </Router>
     </div>
   );
 }
