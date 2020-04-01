9bd8b8a08f3d939db9ea9616e0f8d8aab7603ba5
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-04-01 19:07:15 -0400

    Add missing file

---
+++ ppe-reuze/src/components/Login.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Authenticator, SignIn, SignUp, ForgotPassword } from 'aws-amplify-react';
+import { UserConsumer } from './UserContext';
+import { Redirect } from 'react-router-dom';
+
+const signUpConfig = {
+    header: 'Sign Up for Healthcare Providers',
+    hideAllDefaults: true,
+    defaultCountryCode: '1',
+    signUpFields: [
+      {
+        label: 'Username (public)',
+        key: 'username',
+        required: true,
+        displayOrder: 1,
+        type: 'string'
+      },
+      {
+        label: 'Email',
+        key: 'email',
+        required: true,
+        displayOrder: 2,
+        type: 'string'
+      },
+      {
+        label: 'Password',
+        key: 'password',
+        required: true,
+        displayOrder: 3,
+        type: 'password'
+      },
+      {
+        label: 'NPI',
+        key: 'npi',
+        required: false,
+        displayOrder: 4,
+        type: 'string'
+      },
+    ]
+  };
+
+class Login extends Component {
+    render() {
+        return (
+            <UserConsumer>
+                {({ isLoggedIn, user }) => (
+                    <div>
+                        {isLoggedIn && user ? (
+                            <Redirect to="/start" />
+                        ) : (
+                                <Authenticator signUpConfig={signUpConfig}>
+                                </Authenticator>
+
+                            )}
+                    </div>
+                )}
+            </UserConsumer>
+        )
+    }
+}
+
+export default Login;
