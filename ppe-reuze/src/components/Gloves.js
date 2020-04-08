271f6ec4315fcf68791f5190fb894efa237ca184
Arjun Dayal <62475579+arjundayal94@users.noreply.github.com>
2020-04-08 19:12:59 -0400

    Add files via upload

--- ppe-reuze/src/components/Gloves.js
+++ ppe-reuze/src/components/Gloves.js
@@ -16,15 +16,27 @@ function TypeOfGloves() {
             <div className="columns">
                 <div className="column">
                     <Link to="/submissions/nitrile">
-                        <div className="box">Nitrile</div>
+                        <div className="box">                         
+                            <img src="nitrile_glove.png" width="60" alt="Nitrile Glove" />
+                        <br />
+                        Nitrile 
+                        </div>
+                        <br />
                     </Link>
                 </div>
+        
                 <div className="column">
                     <Link to="/submissions/latex">
-                        <div className="box">Latex</div>
+                        <div className="box">
+                            <img src="latex_glove.png" width="60" alt="Latex Glove" />
+                        <br />
+                        Latex
+                        </div>
+                        <br />
                     </Link>
                 </div>
             </div>
+            
             <Link to="/start">
             <button className="button is-rounded is-info">
                 <i class="fas fa-arrow-left"></i>&nbsp;<span>Back</span>
