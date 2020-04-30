b02e2eaa999eab3a0d84de3ac6da464c7737a6c5
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-04-30 17:53:04 -0400

    Update homepage style

--- ppe-reuze/src/components/Home.js
+++ ppe-reuze/src/components/Home.js
@@ -6,16 +6,22 @@ export default function Home() {
 		<section className="hero is-info is-fullheight-with-navbar">
 			<div class="hero-body">
 				<div className="container">
-					<img src="reuze.png" width="150" />
-					<h1 className="title">Reusing &amp; sanitizing PPE</h1>
-					<h2 className="subtitle">Reuze is an app for healthcare professionals to easily access community-sourced, evidence based strategies for sanitizing personal protect equipment in the COVID-19 epidemic. As a verified healthcare professional, you can also submit your own strategies.</h2>
-					<Link to="/start">
-						<button className="button is-rounded">Get Started</button>
-					</Link>
-					<br />
-					<br />
-					<br />
-					<a href="https://datavant.com/pandemic-response-hackathon/" target="_blank"><img src="hackathon_ribbon.png" width="300" /></a>
+					<div className="columns is-vcentered">
+						<div className="column is-one-quarter">
+							<img src="reuze.png" width="200" />
+						</div>
+						<div className="column">
+							<h1 className="title is-1">Reusing PPE</h1>
+							<h2 className="subtitle is-4">Access and share strategies for sanitizing personal protective equipment in the COVID-19 pandemic.</h2>
+							<Link to="/start">
+								<button className="button is-rounded">Get Started</button>
+							</Link>
+							<br />
+							<br />
+							<br />
+							<span className="tag is-warning is-small"><a href="https://datavant.com/pandemic-response-hackathon/" className="has-text-dark">A Pandemic Response Hackathon Spotlight Project</a></span>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
