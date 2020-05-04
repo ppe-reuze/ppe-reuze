aba331b0f285739bd347f46a1e01e90f234fc39e
Arjun Dayal <arjundayal94@gmail.com>
2020-05-03 22:17:41 -0400

    Update Home.js

--- ppe-reuze/src/components/Home.js
+++ ppe-reuze/src/components/Home.js
@@ -8,18 +8,22 @@ export default function Home() {
 				<div className="container">
 					<div className="columns is-vcentered">
 						<div className="column is-one-quarter">
-							<img src="reuze.png" width="200" />
+							<img src="reuze.png" width="300" />
 						</div>
 						<div className="column">
-							<h1 className="title is-1">Reusing PPE</h1>
-							<h2 className="subtitle is-4">Access and share strategies for sanitizing personal protective equipment in the COVID-19 pandemic.</h2>
+							<h1 className="title is-1">Can I reuse PPE?</h1>
+							<h2 className="subtitle is-4">Reuze allows you to access and share strategies for sanitizing and reusing personal protective equipment. Made for the COVID-19 pandemic.</h2>
+        	                   <span className="tag is-warning is-small"><a href="https://datavant.com/pandemic-response-hackathon/" className="has-text-dark">A Pandemic Response Hackathon Spotlight Project</a></span>
+                            <br />
+							<br />
+                            <br />
 							<Link to="/start">
 								<button className="button is-rounded">Get Started</button>
 							</Link>
 							<br />
 							<br />
 							<br />
-							<span className="tag is-warning is-small"><a href="https://datavant.com/pandemic-response-hackathon/" className="has-text-dark">A Pandemic Response Hackathon Spotlight Project</a></span>
+							
 						</div>
 					</div>
 				</div>
